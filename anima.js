8f9cd9850b3f1723aa341c5fe1d2026dbf626346
HinaM <55233191+HinaM@users.noreply.github.com>
2023-03-12 23:42:51 +0800

    123

--- anima.js
+++ anima.js
@@ -4,7 +4,6 @@ var preload = document.getElementById("preload");
 })
 
 window.onbeforeunload = function(){
-  //刷新后页面自动回到顶部
   document.documentElement.scrollTop = 0;  //ie下
   document.body.scrollTop = 0;  //非ie
 }
