a9a91e470356e825e2dc604d88c40b9f55fe1637
TsaiShuangLan <joannelan88@gmail.com>
2023-03-15 14:00:50 +0800

    number ani

--- anima.js
+++ anima.js
@@ -1,3 +1,4 @@
+/*preload page*/
 var preload = document.getElementById("preload");
     window.addEventListener('load',function(){
         preload.style.display = "none";
@@ -8,6 +9,7 @@ window.onbeforeunload = function(){
   document.body.scrollTop = 0;  //非ie
 }
 
+/* ahwe animation*/
 let boxes = document.querySelectorAll(".ahhwe");
 
 window.addEventListener("scroll", checkBoxes);
@@ -15,7 +17,7 @@ checkBoxes();
 
 function checkBoxes() {
   console.log(window.innerHeight);
-  let triggerBottom = window.innerHeight * 0.8;
+  let triggerBottom = window.innerHeight * 0.7;
   boxes.forEach((ahhwe) => {
     let boxTop = ahhwe.getBoundingClientRect().top;
     if (boxTop < triggerBottom) {
@@ -26,6 +28,26 @@ function checkBoxes() {
   });
 }
 
+/*number animation */
+let boxes1 = document.querySelectorAll(".number");
+
+window.addEventListener("scroll", shownumber);
+shownumber();
+
+function shownumber() {
+  console.log(window.innerHeight);
+  let triggerBottom1 = window.innerHeight * 0.6;
+  boxes1.forEach((number) => {
+    let boxTop1 = number.getBoundingClientRect().top;
+    if (boxTop1 < triggerBottom1) {
+      number.classList.add("ani");
+    } else {
+      number.classList.remove("ani");
+    }
+  });
+}
+
+/* cursor*/
 const cursor = document.getElementById("cursor");
     const positionElement = (e)=> {
         const mouseY = e.clientY;
